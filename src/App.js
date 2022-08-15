f0db253cb612bfa4b20b33f243e42f65e77098a8
Rory Conlon <roryconlon1@gmail.com>
2022-08-15 15:30:16 +0100

    1st draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {name: "Walk Dog", priority: "high"}, 
+    {name: "Water Plant", priority:"low"},
+    {name: "Phone Dentist", priority: "high"}
+  ])
+
+  const [newTask, setNewTask] = useState("");
+  const [newPriority, setNewPriority] = useState("");
+
+  const taskNodes = tasks.map((task, index) => {
+    return <li key={index}>
+      <span>{task.name} {task.priority}</span>
+      </li>
+  })
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const handleTaskPriority = (event) => {
+    setNewPriority(event.target.value);
+  }
+
+
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks]
+    copyTasks.push({name: newTask, priority: newPriority})
+    setTasks(copyTasks);
+    setNewTask("");
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Task List</h1>
+      <hr></hr>
+
+      <form onSubmit={saveNewTask}>
+        <label htmlFor="new-task">Add Task:</label>
+        <input id="new-task" type="text" value={newTask} onChange={handleTaskInput} ></input>
+        <input type="submit" value="Save New task" ></input>
+        <input type="radio" value="High" name="priority"  onChange={handleTaskPriority}></input>High
+        <input type="radio" value="Low" name="priority" onChange={handleTaskPriority}></input>Low
+      </form>
+
+      <ul>
+        {taskNodes}
+      </ul>
+
+
+     
     </div>
   );
 }
